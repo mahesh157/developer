69cb89e5d9a25cb44307c6476fd9100cee4cfdd5
Mahesh <sainimaheshsaini97@gmail.com>
2024-02-27 23:03:36 +0530

    add file

---
+++ index.php
@@ -0,0 +1,118 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Vision World Tech. tech. Pvt. </title>
+    <link rel="icon" type="image/ico" href="img/favicon.png">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container d-flex align-items-center">
+            <!-- <p class="email m-0" style="text-white">Email: admin@visionworldtech.com </p> -->
+            <p class="email m-0" style="color: white;">Email :<a href="mailto:admin@visionworldtech.com" style="color: white;"> admin@visionworldtech.com</a> </p>
+            <p class="phone mb-0" style="color: white;">Phone :<a href="tel:0141-2722370" style="color: white;"> 0141-2722370</a></p>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                <div class="icon-div"><a href="#"><i class="fa-brands fa-facebook" style="color: blueviolet;"></i></a>
+                </div>
+                <div class="icon-div"><a href="#"><i class="fa-brands fa-instagram" style="color: blueviolet;"></i></a>
+                </div>
+                <div class="icon-div"><a href="#"><i class="fa-brands fa-google" style="color: blueviolet;"></i></a>
+                </div>
+                <div class="icon-div"><a href="#"><i class="fa-brands fa-linkedin-in"
+                            style="color: blueviolet;"></i></a></div>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container-fluid background   pt-5">
+        <div class="container">
+            <div class="row flex-column flex-sm-row">
+                <div class="col-sm-6 col-12 text-center py-sm-0 py-4">
+                    <a href="https://facebook.com" target="_blank">
+                        <img src="img/facebook.webp" style="border-radius: 20px;" width="200px" height="120px" alt="">
+                    </a>
+                </div>
+                <div class="col-sm-6 col-12 text-center py-sm-0 py-4">
+                    <a href="https://instagram.com" target="_blank">
+                        <img src="img/instagram.webp" style="border-radius: 20px;" width="200px" height="120px" alt="">
+                    </a>
+                </div>
+            </div>
+            <div class="row py-sm-5 flex-column flex-sm-row ">
+                <div class="col-sm-6 col-12 text-center py-sm-0 py-4">
+                    <a href="https://google.com" target="_blank">
+                        <img src="img/google.webp" style="border-radius: 20px;" width="200px" height="120px" alt="">
+                    </a>
+                </div>
+                <div class="col-sm-6 col-12 text-center py-sm-0 py-4">
+                    <a href="https://linkedin.com" target="_blank">
+                        <img src="img/linkedin.jpg" style="border-radius: 20px;" width="200px" height="120px" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <img class="w-75 img" src="img/logo.jpeg" alt="" srcset="">
+                    <p
+                        style="line-height:20px  ; padding-right:20px !important; font-size: 16px !important; color: aliceblue; font-size: 15px;">
+                        World of Technology is fascinating when we experience it with Open Hearts. So we believe in
+                        providing a world-class experience to our clients.</p>
+                    <div class="d-flex mb-5  ">
+                        <div class="icons-div px-2"><a href="#"><i class="fa-brands fa-facebook-f"
+                                    style="color: rgb(27, 24, 24) ;"></i></a></div>
+                        <div class="icons-div"><a href="#"><i class="fa-brands fa-twitter"
+                                    style="color: rgb(27, 24, 24) ;"></i></a></div>
+                        <div class="icons-div"><a href="#"><i class="fa-brands fa-instagram"
+                                    style="color: rgb(27, 24, 24) ;"></i></a></div>
+                        <div class="icons-div"><a href="#"><i class="fa-brands fa-youtube "
+                                    style="color: rgb(27, 24, 24) ;"></i></a></div>
+
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="logo text-center ">
+                        <img class="w-50" src="img/certificate.png" alt="" srcset="">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <!-- <p class="fw-bolder my-2 pb-4 text-light" style="font-size: 16px">Contact us</p> -->
+                    <p class="fw-bolder my-2 pb-4 text-light" style="font-size: 20px;">Contact us</p>
+                    <p class="font"><i class="fa-solid fa-phone "> <span class="ps-3">0141-2722370</span></i></p>
+                    <p class="font"><i class="fa-regular fa-envelope"></i> <span
+                            class="ps-3">admin@visionworldtech.com</span></p>
+                    <p class="font"><i class="fa-solid fa-house-chimney"></i> <span class="ps-3 lh-lg"> A-15 Jai Ambey
+                            Nagar in front of Jaipur Hospital Tonk Road Jaipur Rajasthan 302018</span></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="text-center last  bg-black mb-0 text-light ">
+        <p>Copyright ©
+            <?=date('Y')?>
+        </p>
+        <p>Designed by VISION WORLD TECH. PVT. LTD.</p>
+    </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
